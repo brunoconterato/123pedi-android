f9d92015ab28f9cbd82492fe9dcb0197450ca426
Bruno Conterato <brunoconterato@gmail.com>
2017-02-28 02:29:34 -0300

    Início: authentication com facebook (falhando na segunda tentativa) e views inicial e de login com Facebook implementadas

---
+++ app/src/main/java/beer/happy_hour/drinking/MainActivity.java
@@ -0,0 +1,38 @@
+package beer.happy_hour.drinking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.*;
+
+import com.facebook.login.widget.LoginButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LoginButton b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        b = (LoginButton) findViewById(R.id.login_button);
+//
+//        b.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+
+    }
+
+    public void login(View view){
+        //Pode haver problema nesse this
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+
+}
